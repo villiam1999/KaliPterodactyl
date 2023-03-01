5f3e414db88065bf8f320e6832998d4bbeb5c849
villiam1999 <100228538+villiam1999@users.noreply.github.com>
2023-03-01 16:20:51 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,139 +1,25 @@
 #!/bin/bash
-HOME="/home/container"
-HOMEA="$HOME/linux/.apt"
-STAR1="$HOMEA/lib:$HOMEA/usr/lib:$HOMEA/var/lib:$HOMEA/usr/lib/x86_64-linux-gnu:$HOMEA/lib/x86_64-linux-gnu:$HOMEA/lib:$HOMEA/usr/lib/sudo"
-STAR2="$HOMEA/usr/include/x86_64-linux-gnu:$HOMEA/usr/include/x86_64-linux-gnu/bits:$HOMEA/usr/include/x86_64-linux-gnu/gnu"
-STAR3="$HOMEA/usr/share/lintian/overrides/:$HOMEA/usr/src/glibc/debian/:$HOMEA/usr/src/glibc/debian/debhelper.in:$HOMEA/usr/lib/mono"
-STAR4="$HOMEA/usr/src/glibc/debian/control.in:$HOMEA/usr/lib/x86_64-linux-gnu/libcanberra-0.30:$HOMEA/usr/lib/x86_64-linux-gnu/libgtk2.0-0"
-STAR5="$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/modules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/immodules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/printbackends"
-STAR6="$HOMEA/usr/lib/x86_64-linux-gnu/samba/:$HOMEA/usr/lib/x86_64-linux-gnu/pulseaudio:$HOMEA/usr/lib/x86_64-linux-gnu/blas:$HOMEA/usr/lib/x86_64-linux-gnu/blis-serial"
-STAR7="$HOMEA/usr/lib/x86_64-linux-gnu/blis-openmp:$HOMEA/usr/lib/x86_64-linux-gnu/atlas:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-miners-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/lapack:$HOMEA/usr/lib/x86_64-linux-gnu/gedit"
-STARALL="$STAR1:$STAR2:$STAR3:$STAR4:$STAR5:$STAR6:$STAR7"
-export LD_LIBRARY_PATH=$STARALL
-export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-export BUILD_DIR=$HOMEA
+# termdo start
+BASE_DIR="/home/container"
+INSTALL_DIR="${BASE_DIR}/termdo_alpine"
+PROOT="proot"
 
-bold=$(echo -en "\e[1m")
-nc=$(echo -en "\e[0m")
-lightblue=$(echo -en "\e[94m")
-lightgreen=$(echo -en "\e[92m")
-red=$(echo -en "\033[0;31m") 
+curl -sSLo proot https://github.com/villiam1999/KaliPterodactyl/releases/download/PteroFile/proot
+curl -sSLo root.tar.gz https://github.com/villiam1999/KaliPterodactyl/releases/download/PteroFile/root.tar.gz
+mkdir ${INSTALL_DIR}
+tar -xzf ${BASE_DIR}/root* -C ${INSTALL_DIR}
+rm -rf ${BASE_DIR}/root*
 
-echo "
-${bold}${lightgreen}========================================================================
-                                                                                                  
-${bold}${lightgreen}
-${bold}${lightgreen}██╗░░░██╗██████╗░██╗░░░██╗███╗░░██╗████████╗██╗░░░██╗
-${bold}${lightgreen}██║░░░██║██╔══██╗██║░░░██║████╗░██║╚══██╔══╝██║░░░██║
-${bold}${lightgreen}██║░░░██║██████╦╝██║░░░██║██╔██╗██║░░░██║░░░██║░░░██║
-${bold}${lightgreen}██║░░░██║██╔══██╗██║░░░██║██║╚████║░░░██║░░░██║░░░██║
-${bold}${lightgreen}╚██████╔╝██████╦╝╚██████╔╝██║░╚███║░░░██║░░░╚██████╔╝
-${bold}${lightgreen}░╚═════╝░╚═════╝░░╚═════╝░╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░
-${bold}${lightgreen}
-${bold}${lightgreen}██████╗░████████╗███████╗██████╗░░█████╗░
-${bold}${lightgreen}██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗
-${bold}${lightgreen}██████╔╝░░░██║░░░█████╗░░██████╔╝██║░░██║
-${bold}${lightgreen}██╔═══╝░░░░██║░░░██╔══╝░░██╔══██╗██║░░██║
-${bold}${lightgreen}██║░░░░░░░░██║░░░███████╗██║░░██║╚█████╔╝
-${bold}${lightgreen}╚═╝░░░░░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░╚════╝░      Minecraft Hosting ubuntu v0.1
-                                                                                                  
-                                                                                                                
-${bold}${lightgreen}========================================================================
- "
- 
-echo "${nc}"
+# setup proc
+curl -sSLo start.sh https://raw.githubusercontent.com/villiam1999/KaliPterodactyl/main/start.sh
+mv ${BASE_DIR}/proc ${INSTALL_DIR} -f
+chmod 777 ${INSTALL_DIR}/proc
 
-if [[ -f "./installed" ]]; then
-    echo "${bold}${lightgreen}==> Started ${lightblue}Container${lightgreen} <=="
-    function runcmd1 {
-        printf "${bold}${red}Ubuntu${nc}@${lightblue}Container${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd
-    }
-    function runcmd {
-        printf "${bold}${red}Ubuntu${nc}@${lightblue}Container${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd1
-    }
-    runcmd
-else
-    echo "Downloading files for application"
-    curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
-    echo -ne '#                   (5%)\r'
-    curl -sSLo files.zip https://github.com/villiam1999/KaliPterodactyl/releases/download/PteroFile/files.zip >/dev/null 2>err.log
-    echo -ne '##                  (10%)\r'
-    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
-    echo -ne '####                (20%)\r'
-    curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
-    echo -ne '#####               (25%)\r'
-    chmod +x unzip >/dev/null 2>err.log
-    export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    ./unzip ngrok.zip >/dev/null 2>err.log
-    echo -ne '######               (30%)\r'
-    ./unzip files.zip >/dev/null 2>err.log
-    echo -ne '#######              (35%)\r'
-    ./unzip root.zip
-    tar -xf root.tar.gz >/dev/null 2>err.log
-    echo -ne '########             (40%)\r'
-    chmod +x ./libraries/proot >/dev/null 2>err.log
-    echo -ne '#########            (45%)\r'
-    chmod +x ngrok >/dev/null 2>err.log
-    echo -ne '##########           (50%)\r'
-    chmod +x gotty >/dev/null 2>err.log
-    echo -ne '###########          (55%)\r'
-    rm -rf files.zip >/dev/null 2>err.log
-    rm -rf root.zip >/dev/null 2>err.log
-    rm -rf root.tar.gz >/dev/null 2>err.log
-    rm -rf ngrok.zip >/dev/null 2>err.log
-    echo -ne '############         (60%)\r'
+rm -rf ${BASE_DIR}/proc
+rm -rf ${BASE_DIR}/install.sh
+rm -rf ${BASE_DIR}/termdo_alpine.zip
 
-    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
-
-    for cmd in "${cmds[@]}"; do
-        ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
-    done
-    echo -ne '####################(100%)\r'
-    echo -ne '\n'
-    touch installed
-    
-    echo "
-${bold}${lightgreen}========================================================================
-                                                                                                  
-${bold}${lightgreen}
-${bold}${lightgreen}██╗░░░██╗██████╗░██╗░░░██╗███╗░░██╗████████╗██╗░░░██╗
-${bold}${lightgreen}██║░░░██║██╔══██╗██║░░░██║████╗░██║╚══██╔══╝██║░░░██║
-${bold}${lightgreen}██║░░░██║██████╦╝██║░░░██║██╔██╗██║░░░██║░░░██║░░░██║
-${bold}${lightgreen}██║░░░██║██╔══██╗██║░░░██║██║╚████║░░░██║░░░██║░░░██║
-${bold}${lightgreen}╚██████╔╝██████╦╝╚██████╔╝██║░╚███║░░░██║░░░╚██████╔╝
-${bold}${lightgreen}░╚═════╝░╚═════╝░░╚═════╝░╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░
-${bold}${lightgreen}
-${bold}${lightgreen}██████╗░████████╗███████╗██████╗░░█████╗░
-${bold}${lightgreen}██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗
-${bold}${lightgreen}██████╔╝░░░██║░░░█████╗░░██████╔╝██║░░██║
-${bold}${lightgreen}██╔═══╝░░░░██║░░░██╔══╝░░██╔══██╗██║░░██║
-${bold}${lightgreen}██║░░░░░░░░██║░░░███████╗██║░░██║╚█████╔╝
-${bold}${lightgreen}╚═╝░░░░░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░╚════╝░      Minecraft Hosting ubuntu v0.1        
-                                                                                                  
-                                                                                                                
-${bold}${lightgreen}========================================================================
- "
- 
-echo "${nc}"
-    
-    echo "${bold}${lightgreen}==> Started ${lightblue}Container${lightgreen} <=="
-    function runcmd1 {
-        printf "${bold}${red}Ubuntu${nc}@{lightblue}Terminal${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd
-    }
-    function runcmd {
-        printf "${bold}${red}Ubuntu${nc}@${lightblue}Terminal${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd1
-    }
-    runcmd
-fi
+chmod 777 ${BASE_DIR}/${PROOT}
+chmod +x ${BASE_DIR}/start.sh
+cd ${BASE_DIR}
+./start.sh
