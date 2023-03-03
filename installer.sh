b3af153c974b9a8df4b9528a3f113c5df2f01c7f
villiam1999 <100228538+villiam1999@users.noreply.github.com>
2023-03-03 22:17:33 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -47,7 +47,7 @@ if [[ -f "./installed" ]]; then
 else
     echo "Telechargement en cours... (0%)"
     wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/Kali/amd64/kali-rootfs-amd64.tar.xz
-    ./unzip kali-rootfs-amd64.tar.xz
+    linux/usr/bin/unzip kali-rootfs-amd64.tar.xz
     wget https://github.com/villiam1999/KaliPterodactyl/releases/download/PteroFile/proot
     echo "Telechargement en cours... (50%)"
     curl -sSLo apth https://cdn2.mythicalkitten.com/pterodactylmarket/ptero-vm/apth
