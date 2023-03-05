04f48da2753db513f60f12bb86ca6dc05d7a3314
villiam1999 <100228538+villiam1999@users.noreply.github.com>
2023-03-05 17:58:20 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-chmod u+x dash
-./dash
+echo "Скачивание системы... (0%)"
+curl -sSLo root.tar.xz https://github.com/RiSecID/AndronixOrigin/blob/master/Rootfs/Parrot/amd64/parrot-rootfs-amd64.tar.xz
+echo "Скачивание необходимых компонентов... (20%)"
+curl -sSLo proot https://github.com/villiam1999/KaliPterodactyl/raw/main/proot
+chmod u+x proot
+echo "Скачивание необходимых компонентов... (50%)"
+curl -sSLo xz https://github.com/villiam1999/KaliPterodactyl/raw/main/xz
+chmod u+x xz
+echo "Установка системы... (100%)"
+tar -I ./xz -xf ./root.tar.xz
+echo "Установка завершена!"
+rm -rf xz
+rm -rf root.tar.xz
+./proot -S . /bin/bash --login
