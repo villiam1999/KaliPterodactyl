c1d4cb575441ced59d303f29abb412536bc739c3
villiam1999 <100228538+villiam1999@users.noreply.github.com>
2023-03-05 15:46:56 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-chmod u+x dash
-./dash
+echo "Установка необходимых файлов (0%)"
+curl -sSLo runimage https://github.com/VHSgunzo/runimage/releases/download/v0.38.4/runimage
+chmod +x runimage*
+echo "Распаковка системы (10%)"
+./runimage --runtime-extract
+echo "Установка системы (50%)"
+cd $HOME/RunDir
+curl -sSLo proot https://github.com/villiam1999/KaliPterodactyl/raw/main/proot
+echo "Запуск системы (100%)"
+chmod +x proot
+./proot -S rootfs bash
