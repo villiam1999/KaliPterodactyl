2ec1f931d6d4add0405c732860712124b6e5b8a1
villiam1999 <100228538+villiam1999@users.noreply.github.com>
2023-03-10 16:29:43 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,20 +5,8 @@ NC='\033[0m'
 AQUA='\033[46m'
 
 if [[ -f "./installed" ]]; then
-echo -e "${AQUA}${BLACK}Добро пожаловать в Ubuntu! Приятного использования ;3"
-    function runcmd1 {
-        printf "UbuntuVDS:~  "
-        read -r cmdtorun
-        ./proot -S . -w /root /usr/bin/env -i MOZ_FAKE_NO_SANDBOX=1 HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=xterm LANG=en_US.UTF-8 LC_ALL=C LANGUAGE=en_US /bin/bash --login
-        runcmd
-    }
-    function runcmd {
-        printf "UbuntuVDS:~ "
-        read -r cmdtorun
-        ./proot -S . -w /root /usr/bin/env -i MOZ_FAKE_NO_SANDBOX=1 HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=xterm LANG=en_US.UTF-8 LC_ALL=C LANGUAGE=en_US /bin/bash --login
-        runcmd1
-    }
-    runcmd
+echo -e "${AQUA}${BLACK}Добро пожаловать в ${RED}Ubuntu! ${BLACK}Приятного использования ;3"
+./proot -S . -w /root /usr/bin/env -i MOZ_FAKE_NO_SANDBOX=1 HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=xterm LANG=en_US.UTF-8 LC_ALL=C LANGUAGE=en_US /bin/bash --login
 else
 echo "Скачивание системы... (0%)"
 curl -sSLo root.tar.gz https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Ubuntu20/focal-amd64.tar.gz
