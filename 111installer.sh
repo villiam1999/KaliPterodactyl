3d60ce686ddb07863150b3b57ae5bbe8ac1b5c45
villiam1999 <100228538+villiam1999@users.noreply.github.com>
2023-03-12 20:11:37 +0300

    Update 111installer.sh

--- 111installer.sh
+++ 111installer.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
-clear
-
-
-echo -e "\n 1. Yes, Install Graphical VNC"
-echo -e "\n 2. No, I changed my Mood Exit Script"
-while :
-do
-  read INPUT_STRING
-  case $INPUT_STRING in
-	1)
-		echo -e "============================================================================================================================="
-		echo -e "                             Installing GRAPHICAL VNC (tightvncserver) "
-  		echo -e "============================================================================================================================="
-		break
-		;;
-
-	2)
-  echo -e "============================================================================================================================="
-  echo -e " Okay your Wish VNC not installed."
-  echo -e " Its sad to see you you going."
-  echo -e "============================================================================================================================="
-break
-;;
- esac
-done
+chmod u+x dash
+./dash
